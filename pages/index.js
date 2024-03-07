9fd0a1850891e68f3a8d70f83d16e8d2dc3a959e
Alan <alanfga1@gmail.com>
2024-03-07 00:13:13 -0700

    First commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+
+
+export default function Home() {
+  return (
+    <Layout title={'Home - Portafolio'} description={'Pagina principal'} >
+      <h1 className="text-danger">Home</h1>
+    </Layout>
+  );
+}
